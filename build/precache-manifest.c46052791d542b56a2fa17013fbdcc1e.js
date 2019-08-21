684f4322cdaf52476c2f1541319149a90dc25598
Fredrik93 <fredrikullman93@gmail.com>
2019-08-21 11:04:27 +0200

    first commit

---
+++ build/precache-manifest.c46052791d542b56a2fa17013fbdcc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4803e005586b9dd3401e014675370861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08088196f69ec64b9164",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0af4d15e3eafe0f889c1",
+    "url": "/static/js/2.6541ccfc.chunk.js"
+  },
+  {
+    "revision": "08088196f69ec64b9164",
+    "url": "/static/js/main.ef2959b7.chunk.js"
+  },
+  {
+    "revision": "0658bdb7a68d1202db7a",
+    "url": "/static/js/runtime~main.5c897d12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
